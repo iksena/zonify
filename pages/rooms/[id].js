38aa73d0dabbafebcbce1f1318bf22feb35f2049
I KOMANG SENA <i.buwana@btpn.com>
2021-08-14 21:36:00 +0700

    Add room tracks page

--- pages/rooms/[id].js
+++ pages/rooms/[id].js
@@ -1,9 +1,53 @@
-import { Row, Col } from 'antd';
+import {
+  Row,
+  Col,
+  List,
+} from 'antd';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-const RoomDetail = () => (
-  <Row>
-    <Col span={24} />
-  </Row>
-);
+import { getTokens } from '../../lib/auth';
+import fetcher from '../../lib/fetcher';
 
-export default RoomDetail;
+const Rooms = () => {
+  const router = useRouter();
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    const { isExpired, accessToken } = getTokens(window);
+    if (isExpired) {
+      router.push('/');
+    }
+
+    const fetchPlaylist = async () => {
+      const response = await fetcher(`http://localhost:3000/api/playlists/${router.query.id}?accessToken=${accessToken || ''}`);
+
+      setTracks(response?.body?.tracks?.items ?? []);
+    };
+    fetchPlaylist();
+  }, [router]);
+
+  return (
+    <Row>
+      <Col span={24}>
+        <List
+          itemLayout="horizontal"
+          dataSource={tracks}
+          renderItem={({ track }) => (
+            <List.Item>
+              <List.Item.Meta
+                title={track.name}
+                description={track.artists?.reduce(
+                  (artist, { name }, index, { length }) => artist.concat(name, index < length - 1 ? ', ' : ''),
+                  '',
+                )}
+              />
+            </List.Item>
+          )}
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default Rooms;
