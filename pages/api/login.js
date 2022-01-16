853db15321eb73a58f524773968519c8a7e7dd80
I Komang Sena <i.buwana@btpn.com>
2022-01-16 13:00:37 +0700

    fix auth scopes

--- pages/api/login.js
+++ pages/api/login.js
@@ -5,19 +5,12 @@ import supabase from '../../lib/supabase';
 const _createSpotifyAuthUrl = ({ spotify, state }, res) => {
   const spotifyAuthUrl = spotify.createAuthorizeURL([
     'user-top-read',
-    'user-read-recently-played',
-    'user-read-playback-state',
-    'app-remote-control',
     'playlist-modify-public',
-    'user-modify-playback-state',
     'playlist-modify-private',
-    'user-read-currently-playing',
     'playlist-read-private',
+    'playlist-read-collaborative',
     'user-read-email',
     'user-read-private',
-    'user-library-read',
-    'playlist-read-collaborative',
-    'streaming',
   ], state && encodeURI(state));
 
   return res.status(200).json({ spotifyAuthUrl });
